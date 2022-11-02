0b325fd40dc27471e6ca56873a233b46186b546d
serge-yudin <vodprava.com@gmail.com>
2022-11-02 18:55:45 +0100

    transition between main and login pages changed

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -13,5 +14,6 @@ def test_guest_can_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(page.browser, page.browser.current_url)
     login_page.should_be_login_page()
