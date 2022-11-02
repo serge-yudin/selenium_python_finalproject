d630c551bc9887da56b20baffbcbf12ac44e4861
serge-yudin <vodprava.com@gmail.com>
2022-11-02 09:36:48 +0100

    conftest.py created

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                    help='Set your desired language for UI')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    language = requests.config.getoption('language')
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
